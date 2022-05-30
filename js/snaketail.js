d9e663f04694f54c2e2dc7278fcc8c1c14c6e58c
Efe Enobakhare <Efe.Enobakhare@hotmail.com>
2022-05-30 22:14:33 +0100

    Snake body getting better

--- js/snaketail.js
+++ js/snaketail.js
@@ -18,39 +18,47 @@ AFRAME.registerComponent('snaketail', {
       let el = this.el;
       let data = this.data;
   
-      //const piMax = 2*Math.PI;
-    //clamp number
-    //const cylinderMin = -0.5;
-    //const cylinderMax = 0.5;
-
-
       document.addEventListener('keyup', event => {
         if (event.code === 'Space') {
-
           
           let scene = document.querySelector('a-scene');
-          //let pos = new THREE.Vector3( data.orbit.object3D.rotation.y, (data.zDist.object3D.position.y+data.orbit.object3D.position.y), el.object3D.position.z);
-          let elthis = new THREE.Vector3( data.orbit.object3D.rotation.y, 1.6, el.object3D.position.z);
-          //let pos = new THREE.Vector3( (data.zDist.object3D.position.y+data.orbit.object3D.position.y),data.orbit.object3D.rotation.y, el.object3D.position.z)
-          // This is correct but needs an extra 1.6 added - data.orbit.object3D.position.y,
-            //pos.y += 
-              
+          //let elthis = new THREE.Vector3( data.orbit.object3D.rotation.y, 1.6, el.object3D.position.z);
+
+            //Create Snake Body
+              data.bodyCount++;
+              let SnakeBodyCount = "Body"+ data.bodyCount
+
               ballbody = document.createElement('a-sphere');
-              
-              //let newpos =  data.zDist.getAttribute("position")
+
               var worldPosition = new THREE.Vector3();
               el.object3D.getWorldPosition(worldPosition)
 
               ballbody.setAttribute('geometry', 'radius', 0.05)
               ballbody.setAttribute('position',worldPosition);
               ballbody.setAttribute('material','color','Firebrick')
-              scene.appendChild(ballbody)                         
-              
-              data.bodyCount++;   
-                 
-              //console.log("World position")
-              //console.log(worldPosition);
+              ballbody.setAttribute('id', SnakeBodyCount )
+              scene.appendChild(ballbody)  
+                            
+              alert(SnakeBodyCount);
           };
       })
+
+
+      document.addEventListener('keyup', event => {
+        if (event.code === 'Enter') {
+          
+          //change colour of snake body black as test
+
+          let SnakeBodyCount = "Body"+ data.bodyCount
+          let SnakeBodyCount_JS = "#"+SnakeBodyCount
+
+            alert(SnakeBodyCount_JS )
+          const sceneEl = document.querySelector('a-scene');
+          let detail = sceneEl.querySelector(SnakeBodyCount_JS).setAttribute('material','color','white') ;                     
+              
+          };
+      })
+
+
     }
   });
