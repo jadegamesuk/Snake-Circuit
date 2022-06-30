8c4d185bf20ed691840f22a6f8a4b7073e813046
Efe Enobakhare <Efe.Enobakhare@hotmail.com>
2022-06-30 16:35:03 +0100

    Scores working! Just need X-axis to be fixed

--- js/apple.js
+++ js/apple.js
@@ -1,40 +1,63 @@
 AFRAME.registerComponent('food', {
     schema: {
-        orbit: {type: 'selector', default: '#gameworld'},
+        orbit: {type: 'selector', default: '#gameworld2'},
         appleCount: {type: 'number', default: 0},
         food: {type: 'selector', default: 'apple'}
     },
 
     init: function () {
-        let el = this.el;
-        let data = this.data;
-
-        let scene = document.querySelector('a-scene');
-        //this.tick = AFRAME.utils.throttleTick(this.tick, 5, this);
-
-        // create random co-ordinate for Apple
-        // ensure no collision with Snake before creating
-
-        //el.object3D.position.set(1, 2, 3)
-
-        // alert("X: " + applePosition.x)
-        // alert("Y: " + applePosition.y )
-        // alert("Z: " + applePosition.z )
-
+        this.tick = AFRAME.utils.throttleTick(this.tick, 10, this);
 
     },
 
     update: function () {
-        let el = this.el;
-        let data = this.data;
-        
-    },
+      },
 
-    remove: function () {
-      // Do something the component or its entity is detached.
-    },
+    tick: function (_t, _dt) {
 
-    tick: function (time, timeDelta) {
-      // Do something on every scene tick or frame.
+      let el = this.el;
+      let data = this.data;
+      let scene = document.querySelector('a-scene');
+
+        el.addEventListener("hitstart", function(event) {
+          //if collision with apple occurs          
+        if (event.target.id)
+        {
+            //delete gltf apple
+            //console.log("removing: " + el.id);
+            el.parentNode.removeChild(el);
+            event.target.id = ""
+
+            //add score
+            data.appleCount++;
+            console.log("SCORE: " + data.appleCount);
+            scoreCounter = "Apple" + data.appleCount;
+
+             //create new apple
+             let entityEl = document.createElement('a-gltf-model');
+             
+             //let entityEl = scene.querySelector('#apple')
+             entityEl.setAttribute('src', '#apple');
+             entityEl.setAttribute('scale', {x: 0.4, y: 0.4, z: 0.4});
+
+             //Create random value for X-axis
+             let xAxis = 0.1;
+
+             // Create random value for Y-axis
+             let yAxis = Math.random() * (2.1 - 1.1) + 1.1;
+
+             //Create random value for Z-axis (0.65 or -0.75)
+             let zAxis = Math.random() < 0.5 ? -0.60 : -0.75;
+
+             entityEl.setAttribute('position', {x: xAxis, y: yAxis, z: zAxis});
+             entityEl.setAttribute('id', '#apple-item' );
+
+             entityEl.setAttribute('food', {orbit: 'player-wrapper2', appleCount: data.appleCount });
+             scene.appendChild(entityEl);
+
+        }
+        });
+
     }
 });
+
