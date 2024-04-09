a980fa86a7c5b533866ea7b7100693c6c2ee9abe
Efe E <efe@jadegames.co.uk>
2024-04-09 14:20:51 +0100

    tes

--- js/fruit-action.js
+++ js/fruit-action.js
@@ -1,24 +1,23 @@
-AFRAME.registerComponent('fruit-action', {
+AFRAME.registerComponent("fruit-action", {
+  init: function () {
+    let el = this.el;
 
-    init: function () {
-        let el = this.el;
+    el.addEventListener("hitstart", function snakeCollisionHandler(event) {
+      //removing event listener
+      el.removeEventListener("hitstart", snakeCollisionHandler);
 
-        el.addEventListener("hitstart", function snakeCollisionHandler(event) {
-            //removing event listener
-            el.removeEventListener('hitstart', snakeCollisionHandler);
+      // Returning Fruit to Pool
+      let FruitpoolName = event.target.id;
+      let poolName = "pool__" + FruitpoolName.substring(6);
+      console.log("============================");
+      console.log("Pool Name: ", poolName);
+      console.log("Fruit Is: ", FruitpoolName);
+      document
+        .querySelector("#" + FruitpoolName)
+        .components[poolName].returnEntity(el);
 
-            // Returning Fruit to Pool
-            let FruitpoolName = event.target.id;
-            let poolName = "pool__" + FruitpoolName.substring(6);
-            console.log("============================")
-            console.log("Pool Name: " , poolName);
-            console.log("Fruit Is: " ,  FruitpoolName );
-            document.querySelector("#" + FruitpoolName ).components[poolName].returnEntity(el);
-
-            //adding event listener
-            el.addEventListener('hitstart', snakeCollisionHandler); 
-
-        });
-
-    }
+      //adding event listener
+      el.addEventListener("hitstart", snakeCollisionHandler);
+    });
+  },
 });
